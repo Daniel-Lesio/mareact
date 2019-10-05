d7239ac2390be506aea911eb6281a159c64eafd2
Daniel Lesio <lesiewiczsales@gmail.com>
2019-10-05 12:40:24 +0200

    change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div className='container'>
-          <h4 className='center'>This is About Page</h4>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos minus ab provident reiciendis unde. Nesciunt id in autem repellat ipsum dolore eaque nemo, dicta vero rerum, itaque deleniti dolor quidem.</p>
+          <h4 className='center'>This is changed About Page</h4>
+          <p>Lorem ipsum dolor sit, ametx consectetur adipisicing elit. Quos minus ab provident reiciendis unde. Nesciunt id in autem repellat ipsum dolore eaque nemo, dicta vero rerum, itaque deleniti dolor quidem.</p>
         </div>
     );
 }
